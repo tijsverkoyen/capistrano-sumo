03c27eb4fcadc5605f5c19eb5c507219156a1729
Tijs Verkoyen <tijs@verkoyen.eu>
2017-07-27 15:28:45 +0200

    Tasks to sync the files

--- lib/capistrano/sumo/defaults.rb
+++ lib/capistrano/sumo/defaults.rb
@@ -5,5 +5,6 @@ end
 
 # Load the tasks
 load File.expand_path('../../tasks/db.rake', __FILE__)
+load File.expand_path('../../tasks/files.rake', __FILE__)
 load File.expand_path('../../tasks/notifications.rake', __FILE__)
 load File.expand_path('../../tasks/redirect.rake', __FILE__)
