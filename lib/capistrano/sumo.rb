f68dc43cc14aec8daf8a4029b41426dffdc37d10
Tijs Verkoyen <tijs@verkoyen.eu>
2017-07-27 14:03:47 +0200

    Notifications
    Added a task to send a notification on a deploy

--- lib/capistrano/sumo.rb
+++ lib/capistrano/sumo.rb
@@ -2,4 +2,5 @@ require 'rake'
 require 'capistrano/sumo/version'
 
 load File.expand_path('../tasks/db.rake', __FILE__)
+load File.expand_path('../tasks/notifications.rake', __FILE__)
 load File.expand_path('../tasks/redirect.rake', __FILE__)
