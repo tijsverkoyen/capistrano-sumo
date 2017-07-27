4d37c4d31110272d0820815a3e349220bc1d2650
Tijs Verkoyen <tijs@verkoyen.eu>
2017-07-27 14:09:45 +0200

    Load defaults
    This will enable us to set some after/before-tasks

---
+++ lib/capistrano/sumo/defaults.rb
@@ -0,0 +1,9 @@
+# Make sure the composer executable is installed
+namespace :deploy do
+  after :finished, "sumo:notifications:deploy"
+end
+
+# Load the tasks
+load File.expand_path('../../tasks/db.rake', __FILE__)
+load File.expand_path('../../tasks/notifications.rake', __FILE__)
+load File.expand_path('../../tasks/redirect.rake', __FILE__)
